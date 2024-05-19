c2f7b9d7945193a643071803a7b8603f210b5eba
hiucko <swimminglizard89@gmail.com>
2024-05-19 14:02:49 +0300

    Revert "un-ttscorvax"
    This reverts commit 08ffbb4836374f363db61916a7964b53bb654629.

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -1,6 +1,7 @@
 using Content.Client.Administration.Managers;
 using Content.Client.Changelog;
 using Content.Client.Chat.Managers;
+using Content.Client.Corvax.TTS;
 using Content.Client.Options;
 using Content.Client.Eui;
 using Content.Client.Flash;
